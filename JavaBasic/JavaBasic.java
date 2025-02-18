989bd5ce2dd2ec4dc17fc3fc18d110898910dd48
sanket Yadav <yadavsanket663@gmail.com>
2025-02-18 21:54:26 +0530

    JavaBasics

---
+++ JavaBasic/JavaBasic.java
@@ -0,0 +1,6 @@
+package JavaBasic;
+public class JavaBasic{
+    public static void main(String arg[]){
+        System.out.println("snaket");
+    }
+}
---
+++ JavaBasic/Swap.java
@@ -0,0 +1,5 @@
+public class Swap{
+    public static void main(String arg[]){
+        System.out.println("Snaket Yadav");
+    }
+}
