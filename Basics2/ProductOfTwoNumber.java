448aa96fc255a3f443f1054756dd145f0300b106
sanket Yadav <yadavsanket663@gmail.com>
2025-02-25 21:08:54 +0530

    Basics

---
+++ Basics2/ProductOfTwoNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ProductOfTwoNumber{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Number ");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c=a*b;
+        System.out.println("The Product of Two Number be "+c);
+
+    }
+}
